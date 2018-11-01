a34886a4c55f1fcb8a4848a88a957c15ba058a7d
qingshuiqianhe <qingshuiqian12@gmail.com>
2018-11-01 14:50:56 +0800

    index

--- django_unittest/urls.py
+++ django_unittest/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from example.views import index
 
 urlpatterns = [
+    url(r'^$', index),
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
 ]
